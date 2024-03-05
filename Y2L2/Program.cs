847337705a69dd7fb42f6c3b8122dc38087d249c
Asus <Asus@DESKTOP-38C8QDK>
2024-03-05 18:13:02 +0200

    Add project files.

---
+++ Y2L2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace CS_L1
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            FileName.Task1();
+
+            FileName.Task2();
+
+            FileName.Task3();
+
+            FileName.Task4();
+
+            FileName.Task5();
+
+            FileName.Task6();
+
+        }
+    }
+}
+
+//internal class Program
+//{
+//    public static void Task1()
+//    {
+//        Console.WriteLine("Task 1:");
+//        Console.WriteLine("\"It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\"");
+//        Console.WriteLine("Bjarne Stroustrup");
+
+//    }
+
+//    public static void Task2()
+//    {
+
+//    }
+
+//    public static void Task3()
+//    {
+
+//    }
+
+//    public static void Task4()
+//    {
+
+//    }
+
+//    public static void Task5()
+//    {
+
+//    }
+//    public static void Task6()
+//    {
+
+//    }
+
+//}
